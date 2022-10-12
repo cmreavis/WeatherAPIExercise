3e09ca5e7676088cf9cab7cd21b46ea3f30bdb36
cmreavis97 <cmreavis97@gmail.com>
2022-10-12 15:45:57 -0400

    changes

--- WeatherAPIExercise/WeatherAPIExercise/Program.cs
+++ WeatherAPIExercise/WeatherAPIExercise/Program.cs
@@ -1,5 +1,31 @@
 ﻿using Newtonsoft.Json.Linq;
-using System.Net.Http;
+
+namespace WeatherAPIExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
 
 
+            //Take API key
+            //Console.WriteLine("Please enter your key:");
+            //Console.ReadLine();
+            var apiKey = "a72a8e95c813f97a891610d899110159";
+            //Take city input
+            //Console.WriteLine("For which city would you like to see the local weather?");
+            //Console.ReadLine();
+            var city = "Mooresville";
 
+            //URL for OpenWeather 
+            //Includes usage of Imperial units, uses city and apiKey variables
+            var openWeatherUrl = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={apiKey}";
+
+            //Stores response in a local variable
+            var weatherResponse = client.GetStringAsync(openWeatherUrl).Result;
+
+
+        }
+    }
+}
